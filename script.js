2219f618be03785db3769c43acc0f7b78ed3e923
mtm90 <mattia22nchi@gmail.com>
2024-05-06 09:42:27 +0200

    fixed times

--- script.js
+++ script.js
@@ -100,11 +100,11 @@ function showCompletionMessage() {
     const elapsedTime = Math.floor((endTime - startTime) / 1000); // Convert milliseconds to seconds
     let message;
 
-    if (elapsedTime < 45) {
+    if (elapsedTime < 60) {
         message = `Congratulations! You completed the game in ${elapsedTime} seconds. That was fast!`;
-    } else if (elapsedTime >= 45 && elapsedTime <= 60) {
+    } else if (elapsedTime >= 60 && elapsedTime <= 90) {
         message = `Congratulations! You completed the game in ${elapsedTime} seconds. That was nice but you can do better!`;
-    } else if (elapsedTime > 60 && elapsedTime <= 90) {
+    } else if (elapsedTime > 90 && elapsedTime <= 120) {
         message = `Congratulations! You completed the game in ${elapsedTime} seconds. That was kinda mid, try again!`;
     } else {
         message = `Congratulations! You completed the game in ${elapsedTime} seconds. That was kinda slow, you can certainly do better!`;
